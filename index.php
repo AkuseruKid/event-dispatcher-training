91dd9f230b8a90140fcdba90bcc70da0b23edf6d
Axel Redois <axelredois25@gmail.com>
2020-03-21 12:28:08 +0100

    final refactoring, creating bootstrap.php

--- index.php
+++ index.php
@@ -1,21 +1,8 @@
 <?php
 
 use App\Controller\OrderController;
-use App\DependancyInjection\EventCompilerPass;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-require __DIR__ . '/vendor/autoload.php';
-
-$container = new ContainerBuilder();
-
-$loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
-$loader->load("services.yaml");
-
-$container->addCompilerPass(new EventCompilerPass());
-
-$container->compile();
+require __DIR__ . '/config/bootstrap.php';
 
 $controller = $container->get(OrderController::class);
 
